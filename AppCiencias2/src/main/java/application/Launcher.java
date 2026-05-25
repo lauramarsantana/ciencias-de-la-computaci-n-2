01f84c473ccf724b283a86a716004c8c5ae38805
lauramarsantana <lmsantanas@udistrital.edu.co>
2026-05-25 02:27:20 -0500

    Fix: Add Launcher to bypass JavaFX runtime graphics issue

---
+++ AppCiencias2/src/main/java/application/Launcher.java
@@ -0,0 +1,8 @@
+package application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Esto arranca la app de JavaFX saltándose la validación estricta de módulos
+        App.main(args);
+    }
+}
